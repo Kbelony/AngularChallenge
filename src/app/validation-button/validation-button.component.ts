86dd756ca63584666058277428f46cb162560584
Kalvin Belony--Sagbo <kalvin.sgb@outlook.com>
2021-12-13 16:24:47 +0100

    Challenge 2 done

---
+++ src/app/validation-button/validation-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-validation-button',
+  templateUrl: './validation-button.component.html',
+  styleUrls: ['./validation-button.component.css']
+})
+export class ValidationButtonComponent implements OnInit {
+
+  constructor() {
+    class ValidationButtonComponent{
+    }
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
